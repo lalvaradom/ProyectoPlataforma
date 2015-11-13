56599d10a44a13bf3d9ccc1459ffcfbe8e4cd2ec
akshay bhat <akshayubhat@gmail.com>
2015-11-13 07:23:13 -0500

    added naive face detection code.

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,8 @@ from fabric.operations import local as lrun, run
 from fabric.state import env
 from settings import BUCKET_NAME
 import data
+import detector
+import time
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -15,7 +17,12 @@ logging.basicConfig(level=logging.INFO,
 @task
 def process():
     dataset = data.Dataset()
+    count  = 0
+    start = time.time()
     for model,key,img in dataset.get_images(BUCKET_NAME):
-        print model,key,img.shape
-        break
+        print model,key,img.shape,detector.detect(img)
+        count +=1
+        if count == 20:
+            print round(time.time() - start,1)
+            break
 
